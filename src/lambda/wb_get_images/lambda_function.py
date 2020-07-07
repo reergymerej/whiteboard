c21060329f12cdd7085a01ecee7a845894c2b36e
Jeremy Greer <jex.grizzle@gmail.com>
2020-07-07 16:14:58 -0700

    Add full image path

--- src/lambda/wb_get_images/lambda_function.py
+++ src/lambda/wb_get_images/lambda_function.py
@@ -1,13 +1,34 @@
 import json
+import boto3
+
+def get_images():
+    client = boto3.client('s3')
+    response = client.list_objects_v2(
+        Bucket='jex-whiteboard',
+    )
+    images = []
+    for ob in response.get('Contents', []):
+        name = ob.get('Key')
+        if name[0] is 'I':
+            modified = ob.get('LastModified')
+            images.append({
+                'name': 'https://jex-whiteboard.s3.amazonaws.com/' + name,
+                'modified': modified.timestamp(),
+            })
+
+    return images
 
 def lambda_handler(event, context):
+    images = get_images()
     return {
         "statusCode": 200,
         "headers": {
             "Access-Control-Allow-Origin":"*",
             "Content-Type": "application/json"
         },
-        'body': json.dumps([
-            'https://jex-whiteboard.s3.amazonaws.com/IMG_3556.JPG',
-        ])
+        'body': json.dumps(images)
     }
+
+if __name__ == '__main__':
+    result = lambda_handler(None, None)
+    print(result)
